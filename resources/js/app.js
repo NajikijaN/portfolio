46ccc158d10f8cd79eef11dd044b811a47f1e88d
NajikijaN <159284180+NajikijaN@users.noreply.github.com>
2026-04-21 13:49:33 +0200

    Refactor code structure for improved readability and maintainability

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-//
+const buttons = document.querySelectorAll('.button-container button');
+const projectLinks = document.querySelectorAll('.project-link');
+const descriptionElement = document.getElementById('project-description');
+const descriptions = {
+    viewsource: 'ViewSource is een open-source project dat ik heb ontwikkeld met Laravel. Met ViewSource kunnen gebruikers gemakkelijk een URL invoeren en de broncode van die pagina bekijken. Het project is ontworpen om educatief te zijn, zodat gebruikers kunnen leren hoe webpagina\'s zijn opgebouwd en hoe ze werken. Probeer het zelf uit door een URL in te voeren en de broncode te verkennen!',
+    nude: 'Website ontwikkeld voor Hollandica B.V. met een professionele uitstraling en een gebruiksvriendelijke interface. De website is ontworpen om klanten een kijkje te laten nemen van en in B&B De Oude Nude. Ook kunnen klanen gemakkelijk contact opnemen en reserveringen maken.',
+    hollandica: ''
+};
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        const project = button.getAttribute('data-project');
+        projectLinks.forEach(link => link.classList.remove('active'));
+
+        const activeProjectLink = document.querySelector(`.project-link[data-project="${project}"]`);
+        if (activeProjectLink) {
+            activeProjectLink.classList.add('active');
+        }
+
+        descriptionElement.textContent = descriptions[project] || '';
+    });
+});
